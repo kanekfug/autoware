d70af66b82207057def5932fcd3def103b2ac5b9
kanekfug <kanekfug@fsi.co.jp>
2026-01-28 15:42:08 +0900

    Update run.sh

--- .webauto-ci/main/autoware-setup/run.sh
+++ .webauto-ci/main/autoware-setup/run.sh
@@ -2,8 +2,6 @@
 
 : "${WEBAUTO_CI_GITHUB_TOKEN:?is not set}"
 
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo "$UBUNTU_CODENAME") main" | sudo tee /etc/apt/sources.list.d/ros2.list >/dev/null
 sudo -E apt-get -y update
 sudo -E apt-get -y install usbutils # For kvaser
 
